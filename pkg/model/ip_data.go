2cb9c9336618f0c49c0a5a9341d6d8aa0760c54b
Pablo Cuenca <108037606+ccxnu@users.noreply.github.com>
2024-06-06 14:26:11 -0500

    Add model for ipapi data

---
+++ pkg/model/ip_data.go
@@ -0,0 +1,18 @@
+package model
+
+type IPData struct {
+	IP          string  `json:"ip"`
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	Region      string  `json:"region"`
+	RegionName  string  `json:"regionName"`
+	City        string  `json:"city"`
+	Zip         string  `json:"zip"`
+	Lat         float64 `json:"lat"`
+	Lon         float64 `json:"lon"`
+	Timezone    string  `json:"timezone"`
+	ISP         string  `json:"isp"`
+	Org         string  `json:"org"`
+	Proveedor   string  `json:"as"`
+	UserAgent   string  `json:"userAgent"`
+}

487953c4de8232fd008c7017d49cff0a71976ce1
Pablo Cuenca <108037606+ccxnu@users.noreply.github.com>
2024-06-06 16:42:34 -0500

    Update logic'

--- internal/service/mysql_service.go
+++ internal/service/mysql_service.go
@@ -9,19 +9,25 @@ import (
 	"github.com/ccxnu/ips-redis-mysql/pkg/model"
 )
 
-func SaveToDatabase(data *model.IPData) error {
+func SaveToDatabase(data *model.IPData) (bool, error) {
 
 	db, err := database.NewMysqlConnection(&config.AppConfig)
 	if err != nil {
-		return err
+		log.Fatalf("Error connecting to the database: %v", err)
 	}
 
 	defer db.Close()
 
-	querySearh := `SELECT ip FROM GeoData WHERE ip = ?`
-	if err := db.QueryRow(querySearh, data.IP).Scan(); err != sql.ErrNoRows {
-		log.Printf("IP %s already exists in the database", data.IP)
-		return nil
+	// Check if the IP already exists in the SaveToDatabase
+	ipExists, err := GetInfoById(db, data.IP)
+	if err != nil {
+		log.Printf("Error checking if IP exists in the database: %v", err)
+		return false, err
+	}
+
+	// Poco pirata esto
+	if ipExists {
+		return true, err
 	}
 
 	query := `INSERT INTO GeoData (ip, country, countryCode, region, regionName,
@@ -32,8 +38,22 @@ func SaveToDatabase(data *model.IPData) error {
 	_, err = db.Exec(query, data.IP, data.Country, data.CountryCode, data.Region, data.RegionName, data.City, data.Zip, data.Lat, data.Lon, data.Timezone, data.ISP, data.Org, data.Proveedor, data.UserAgent)
 
 	if err != nil {
-		return err
+		log.Printf("Error saving data to database: %v", err)
+		return false, err
 	}
 
-	return nil
+	return false, nil
+}
+
+func GetInfoById(db *database.DB, ip string) (bool, error) {
+	ipExists := false
+
+	querySearh := `SELECT ip FROM GeoData WHERE ip = ?`
+	if err := db.QueryRow(querySearh, ip).Scan(); err != sql.ErrNoRows {
+		log.Printf("IP %s already exists in the database", ip)
+		ipExists = true
+		return ipExists, nil
+	}
+
+	return ipExists, nil
 }
